0fdc6e601c1083364aedffea395dea1aa1a728a0
Maya Kumaran <mk953@cornell.edu>
2020-12-14 21:27:22 -0500

    HW19

---
+++ index.js
@@ -0,0 +1,6 @@
+$( "#button1" ).mouseover(function() {
+    $(this).css("background-color", "#FF9B85")
+});
+$( "#button1" ).mouseout(function() {
+    $(this).css("background-color", "#EE6055")
+});
